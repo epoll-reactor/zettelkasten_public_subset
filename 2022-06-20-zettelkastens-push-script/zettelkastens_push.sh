c7bc2e2ec7df773ba07af80ab198215268d0c2f7
epoll-reactor <>
2022-06-20 22:15:56 +0200

    Update.

--- 2022-06-20-zettelkastens-push-script/zettelkastens_push.sh
+++ 2022-06-20-zettelkastens-push-script/zettelkastens_push.sh
@@ -8,15 +8,3 @@ function push {
 
 push $HOME/git/zettelkasten 'update'
 push $HOME/git/zettelkasten_public_subset 'Update.'
-
-# pushd $HOME/git/zettelkasten
-# git add *
-# git commit -m 'update'
-# git push -f
-# popd
-
-# pushd $HOME/git/zettelkasten_public_subset
-# git add *
-# git commit -m 'Update.'
-# git push -f
-# popd
