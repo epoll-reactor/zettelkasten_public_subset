9641bdadfeaadd4e2122b675c94e9dffa31ad00e
epoll-reactor <>
2022-08-02 00:11:54 +0200

    Update.

--- 2022-08-02-C++-operator[]-multiple-parameters/main.cpp
+++ 2022-08-02-C++-operator[]-multiple-parameters/main.cpp
@@ -2,9 +2,23 @@
 
 enum x {_1=100,_2=200};
 
-std::pair<enum x, enum x> operator,(enum x lhs, enum x rhs) {
+struct pair_wrapper {
+	std::pair<enum x, enum x> pair_;
+	pair_wrapper(std::pair<enum x, enum x> pair)
+		: pair_(pair) {}
+	operator std::pair<enum x, enum x>() const {
+		return pair_;
+	}
+};
+
+pair_wrapper operator,(enum x lhs, enum x rhs) {
 	return std::make_pair(lhs, rhs);
 }
+pair_wrapper operator,(pair_wrapper wrapper, enum x rhs) {
+	return std::make_pair(
+		wrapper.pair_.first, rhs);
+}
+
 
 struct comma_operator_test {
 	int operator[](std::pair<enum x, enum x> pair) {
@@ -14,5 +28,5 @@ struct comma_operator_test {
 
 int main() {
 	comma_operator_test test;
-	return test[_1,_2];
+	return test[_1,_2,_1,_2,_1,_2,_1];
 }
